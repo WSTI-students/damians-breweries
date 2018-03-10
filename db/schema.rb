d8d14b22335dc4094470745d9875f8153141c0dd
Duman <damian814@onet.eu>
2018-03-10 14:37:17 +0100

    Add devise model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310150748) do
+ActiveRecord::Schema.define(version: 20180310132759) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180310150748) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_ingredients_on_user_id"
+
   end
 
 end
