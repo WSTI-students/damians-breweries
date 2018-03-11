32991645528e13abb626c88fa0d15761d32d9b6c
Naimad Agresto <damian.ostrega@gmail.com>
2018-03-11 14:19:23 +0100

    Adds column 'uniit' to Ingredients table

--- db/migrate/20180310150748_ingredients_table.rb
+++ db/migrate/20180310150748_ingredients_table.rb
@@ -4,6 +4,7 @@ class IngredientsTable < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :category 
       t.references :user
+      t.string :unit
       t.timestamps
     end
   end
