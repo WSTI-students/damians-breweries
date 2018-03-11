371849576001acca55d006dd7473e602c998ff3d
Naimad Agresto <damian.ostrega@gmail.com>
2018-03-11 12:56:13 +0100

    Adds fixes from Ozim comments :)

--- db/migrate/20180310150748_ingredients_table.rb
+++ db/migrate/20180310150748_ingredients_table.rb
@@ -1,11 +1,9 @@
 class IngredientsTable < ActiveRecord::Migration[5.1]
   def change
     create_table :ingredients do |t|
-      
       t.string :name
       t.string :category 
       t.references :user
-      
       t.timestamps
     end
   end
