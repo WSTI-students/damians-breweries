28226305234e380704e2f97c18884d3f4bf01817
Damian Śleziona <damian.sleziona@gmail.com>
2018-03-16 16:27:21 +0100

    Change table name

--- db/migrate/20180310162056_create_styles.rb
+++ db/migrate/20180310162056_create_styles.rb
@@ -1,6 +1,6 @@
 class CreateStylesTable < ActiveRecord::Migration[5.1]
   def change
-    create_table :styles_tables do |t|
+    create_table :styles do |t|
       t.string :name
       t.string :description
     end
