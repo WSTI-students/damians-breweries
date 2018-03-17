877f655221fbfcd6da77cf693c761215c2e1496f
Naimad Agresto <damian.ostrega@gmail.com>
2018-03-17 15:46:59 +0100

    Fixes index.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
-
+  
   root to: "users#index"
   resources :recipes
-
+  
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
