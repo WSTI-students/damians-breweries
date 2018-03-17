81be09a35dfb66f07b979efd165ba23c806bea51
Damian Śleziona <damian.sleziona@gmail.com>
2018-03-17 12:05:45 +0100

    Add new recipe form, recipes controller, routes and relations

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,16 @@
 class RecipesController < ApplicationController
+  def new
+  	@recipe = current_user.recipes.new
+  	@styles = Style.all
+  end
+
+  def create
+  	current_user.recipes.create(recipe_params)
+  end
+
+  private
+
+  def recipe_params
+	params.require(:recipe).permit(:name, :description, :style_id, :process_desc)
+  end
 end
