519e6ddb7585e582b6711c9cb8ab5a0efb30fbb7
Naimad Agresto <damian.ostrega@gmail.com>
2018-03-17 10:32:10 +0100

    Resolves conflict in recipe.rb

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,4 @@
 class Recipe < ApplicationRecord
-<<<<<<< 6445bed3165d96e1307a408059f509baa702667e
-end
-=======
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
 end
->>>>>>> Adds corrections after comments
