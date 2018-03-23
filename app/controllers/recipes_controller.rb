866a103d9254e72217e87fd80fe21bd270079e96
Duman <damian814@onet.eu>
2018-03-23 18:10:47 +0100

    Fix wrong indentation

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
-  @recipes = Recipe.all
+    @recipes = Recipe.all
   if params[:search]
     @recipes = Recipe.search(params[:search]).order("created_at DESC")
   else
